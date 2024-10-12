0831bbb92ea8ac0fd51d0e10cd87333b4a00cb5d
Jorge Enrique Torres Mora <jorgetorresmora99@gmail.com>
2024-10-12 14:43:40 -0500

    Implementación del servicio de cierre de sesión

--- js/principal.js
+++ js/principal.js
@@ -7,15 +7,56 @@ window.addEventListener('load',function(){
     const result = JSON.parse(this.localStorage.getItem('result'));
 
     mostrarAlerta(result.nombreUsuario, msgSuccess)
+    const btnCerrar = this.document.getElementById('btnCerrarSesion');
 
+    btnCerrar.addEventListener('click', function(){
+        //console.log(result);
+        cerrarSesion(result.correoUsuario, msgSuccess)
+    })
 })
 
-function mostrarAlerta(mensaje, msgError){
-    msgError.innerHTML = mensaje;
-    msgError.style.display = 'block';
+function mostrarAlerta(mensaje, msg){
+    msg.innerHTML = mensaje;
+    msg.style.display = 'block';
 }
 
-function ocultarAlerta(msgError){
-    msgError.innerHTML = '';
-    msgError.style.display = 'none';
+function ocultarAlerta(msg){
+    msg.innerHTML = '';
+    msg.style.display = 'none';
+}
+
+async function cerrarSesion(email, msg){
+    mostrarAlerta("Cerrando sesión...",msg)
+    const url = 'http://localhost:8082/login/close-async';
+    const data = {
+        email:email
+    };
+    try{
+        const response = await fetch(url,{
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        if(!response.ok){
+            mostrarAlerta('Error! Problema al cerrar sesion');
+            throw new Error(`Error: ${response.statusText}`)
+        }
+
+        const result = await response.json();
+        console.log('Respuesta del server', result);
+
+        if(result.codigo == '00'){
+            localStorage.removeItem('result');
+            window.location.replace('index.html');
+        } else {
+            mostrarAlerta(result.mensaje, msg);
+        }
+
+    }catch (error) {
+        console.error('Error: Problema en el servicio', error)
+        mostrarAlerta('Error: Problema en el servicio', msg)
+    }
 }
